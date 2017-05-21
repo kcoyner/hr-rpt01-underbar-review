318d1c6f1eb56f576ec2458e30e193cdbd0d8370
Kevin Coyner <kevin@rustybear.com>
2017-05-21 14:41:38 -0400

    doodle: doodle:  Complete through _.some.

--- src/underbar.js
+++ src/underbar.js
@@ -196,16 +196,36 @@
     }, false);
   };
 
+  {}_.identity === true;
 
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    if (arguments.length > 1){
+      return _.reduce(collection, function (accumulator, value) {
+        if (!iterator(value)){
+          accumulator = false;
+        }
+        return accumulator;
+      },true);
+    } else {
+      return _.reduce(collection , function (accumulator, value) {
+        if (value !== true) {
+          accumulator = false;
+        }
+      return accumulator;
+      },true);
+    }
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    iterator = iterator || _.identity;
+    return !_.every(collection, function(value){
+      return !iterator(value);
+    });
   };
 
 
